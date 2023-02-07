73a22889d810e13bfa6d10217bf8859d04b26ede
mahekagg <105991553+mahekagg@users.noreply.github.com>
2023-02-07 13:53:16 -0500

    aligned and button

---
+++ main.js
@@ -0,0 +1,9 @@
+function buttonClick() {
+    var color = document.body.style.color
+    
+    if (color == 'black') {
+        document.body.style.color = 'green'
+    } else {
+        document.body.style.color = 'black'
+    }
+}
